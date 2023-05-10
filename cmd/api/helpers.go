788a83dc1fcd0996878dfef57762e7d06a864aa8
xavier <xavier.lis@gmail.com>
2023-05-10 22:59:14 +0300

    3.3 Advanced Json Configuration

---
+++ cmd/api/helpers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+type envelope map[string]interface{}
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid ID parameter")
+	}
+
+	return id, nil
+}
+
+func (app *application) writeJson(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for header, value := range headers {
+		w.Header()[header] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(js)
+
+	return err
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module go-greenlight
+
+go 1.20
+
+require github.com/julienschmidt/httprouter v1.3.0
