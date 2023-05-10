28193c92acd4d0db55417e5b4e37fe6507de9025
xavier <xavier.lis@gmail.com>
2023-05-10 23:15:02 +0300

    3.6 Sending Error Messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,7 +10,7 @@ import (
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponce(w, r)
 		return
 	}
 
@@ -26,9 +26,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
 
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
 
