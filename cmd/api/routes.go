278c31cba3569916a40b42c100810c15d69b36b2
xavier <xavier.lis@gmail.com>
2023-05-14 14:49:32 +0300

    7 CRUD operations

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponce)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:1", app.deleteMovieHandler)
 
 	return router
 }
